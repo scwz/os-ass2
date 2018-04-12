8e5467f89558b57626bc7e3ac3050ffbd47e5b5d
william <wmchan622@gmail.com>
2018-04-12 23:35:59 +1000

    oops

--- kern/syscall/file_syscalls.c
+++ kern/syscall/file_syscalls.c
@@ -30,7 +30,7 @@ sys_open(userptr_t user_filename, int flags, int *retval)
         filename = kmalloc(sizeof(char *) * PATH_MAX);
         result = copyinstr(user_filename, filename, PATH_MAX, &got);
         if (result) {
-                return EFAULT;
+                return result;
         }
 
         result = open(filename, flags, retval);
